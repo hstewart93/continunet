285827d20584ecab909cb5798efd0aea992eea18
hstewart93 <harriet.stt@gmail.com>
2024-04-30 16:49:18 +0100

    Add wheel to build dependencies.

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name="continunet",
     packages=find_packages(include=["continunet"]),
-    version="0.0.1",
+    version="0.0.0",
     description="Source finding package for radio continuum data.",
     author="Hattie Stewart",
     install_requires=[],
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest==4.4.1"],
     test_suite="continunet/tests",
 )
